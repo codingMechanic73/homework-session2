afb3c1653f42a86ca8d43a1e7294f572c2e91983
codingMechanic73 <alanwinston2019@gmail.com>
2020-10-26 16:40:46 +0530

    check if bean for datasource is created

--- src/main/java/com/upgrad/mba/MovieBookingApplication.java
+++ src/main/java/com/upgrad/mba/MovieBookingApplication.java
@@ -2,12 +2,16 @@ package com.upgrad.mba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class MovieBookingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieBookingApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(MovieBookingApplication.class, args);
+		DataSource ds = ctx.getBean(DataSource.class);
 	}
 
 }
